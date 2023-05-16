21f23105b5b52cac886686158b1530b4cc8c49dc
Niks Derums <niks.derums@gmail.com>
2023-05-16 15:35:37 +0300

    Fix download script

--- download_tables.py
+++ download_tables.py
@@ -1,5 +1,4 @@
 from Modules.osp_api_analyzer import OspApiAnalyzer
 
-ospApiAnalyzer = OspApiAnalyzer()
+ospApiAnalyzer = OspApiAnalyzer(table_limit = 1000000, sleep_time=1)
 ospApiAnalyzer.read_categories()
-ospApiAnalyzer.print_categories_tables_with_extra_info()
